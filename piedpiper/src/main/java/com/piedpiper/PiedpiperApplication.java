32056803dbe93e019b6f386b11ebf05e81cb72ba
homeless2010 <15594821882@163.com>
2018-05-08 15:09:12 +0800

    create project

---
+++ piedpiper/src/main/java/com/piedpiper/PiedpiperApplication.java
@@ -0,0 +1,12 @@
+package com.piedpiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedpiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedpiperApplication.class, args);
+	}
+}
